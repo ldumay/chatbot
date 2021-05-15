04fdd1f0b9c02796fb2919cc6b61594a819e984f
Loic DUMAY <dumay.famille@gmail.com>
2021-05-15 23:30:12 +0200

    MAJ - Mineur - Beta 1.2

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//FR" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
     <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <title>LDumay | Projet ChatBot</title>
+        
+        <!-- MetasDatas -->
+        <meta name="Content-Type" content="UTF-8" charset="UTF-8">
+        <meta name="Content-Language" content="fr">
+        <meta content="IE=edge" http-equiv="X-UA-Compatible">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <meta name="description" content="Pour plus de description, n'héistez pas à consulter directement mon site web https://ldumay.fr/. A bientôt !">
-        <meta name="author" content="Mr Loïc Dumay">
+        <meta name="Description" content="Bonjour et Bienvenue à vous ! Je suis heureux de vous voir sur mon site Web personnel. Celui-ci regroupe tout ce dont vous avez besoin de savoir sur moi-même. Bonne visite.<br/>Pour plus de description, n'héistez pas à consulter directement mon site web https://ldumay.fr/. A bientôt !">
+        <meta name="Subject" content="Site Vitrine Personnel">
+        <meta name="Copyright" content="Loïc Dumay (alias LDumay)">
+        <meta name="Author" content="Loïc Dumay (alias LDumay)">
+        <meta name="Publisher" content="Loïc Dumay (alias LDumay)">
+        <meta name="Identifier-Url" content="https://ldumay.fr">
+        <meta name="Reply-To" content="hello@ldumay.fr">
+        <meta name="Revisit-After" content="10 days">
+        <meta name="Robots" content="index">
+        <meta name="Rating" content="general">
+        <meta name="Distribution" content="global">
+        <meta name="Geography" content="Ile-De-France, France">
+        <meta name="Category" content="internet">
+        <!-- ./MetasDatas -->
 
         <!-- Favicon -->
         <link rel="shortcut icon" href="https://dev.ldumay.fr/resources/img/favicons/favicon.png">
@@ -17,18 +32,26 @@
         <link rel="apple-touch-icon" href="https://dev.ldumay.fr/resources/img/favicons/favicon_apple.png">
         <!-- ./Apple Icon -->
 
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Icons -->
+        <link rel="stylesheet" type="text/css" href="https://glyphsearch.com/bower_components/font-awesome/css/all.min.css"/>
 
-        <title>LDumay | Projet ChatBot</title>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/font-awesome/font-awesome.css"/>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/foundation-icons/foundation-icons.css"/>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/ico-moon/ico-moon.css"/>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/ion-icons/ion-icons.css"/>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/material-design-icons/material-design-icons.css"/>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/oct-icons/oct-icons.css"/>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/pe7-stroke-icons/pe-icon-7-stroke.css"/>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/simple-line-icons/simple-line-icons.css"/>
+        <link rel="stylesheet" type="text/css" href="https://dev.ldumay.fr/resources/icons/themify-icons/themify-icons.css"/>
+        <!-- ./Icons -->
 
         <!-- Bootstrap core CSS -->
-        <link href="https://dev.ldumay.fr/toolkits/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
-        <link href="https://dev.ldumay.fr/toolkits/bootstrap/4.1.3/css/bootstrap-grid.min.css" rel="stylesheet">
+        <link href="https://dev.ldumay.fr/resources/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://dev.ldumay.fr/resources/bootstrap/4.1.3/css/bootstrap-grid.min.css" rel="stylesheet">
 
         <!-- GoogleFonts -->
-        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Poiret+One">
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Poiret+One&family=Alatsi&family=Roboto&family=Righteous">
         <!-- ./GoogleFonts -->
 
         <!-- Custom styles for this template -->
